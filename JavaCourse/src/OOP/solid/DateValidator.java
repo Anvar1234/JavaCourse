a269e85ee91908ab2eb0ed97aa4c49a4ebc2303f
Anvar <kingartaved@yandex.ru>
2023-07-13 00:41:22 +0500

    Написал пару методов. Для проверки вложенности скобок, вывода стека, разделения пробелами.

--- JavaCourse/src/OOP/solid/DateValidator.java
+++ JavaCourse/src/OOP/solid/DateValidator.java
@@ -1,8 +1,6 @@
 package OOP.solid;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 /**
  * Проверка пользовательского выражения (ввода) (isNumber).
@@ -11,16 +9,24 @@ import java.util.List;
 public class DateValidator {
     private String expression;
     private List<String> token;
+    private Map<Character, Character> brackets; //1.Во множ числе можно писать? 2.Здесь инициализировать или как я - ниже?
 
     public DateValidator(String expression) {
         this.expression = expression.replaceAll(" ", "").trim();
-        this.token = List.of("+", "-", "/", "*", "(", ")", ".", "0", "1", "2", "3", "4",
-                "5", "6", "7", "8", "9"); // дополнить
+        this.token = List.of("+", "-", "/", "*", "(", ")", "[","]", ".", "0", "1", "2", "3", "4",
+                "5", "6", "7", "8", "9");
+        this.brackets = new HashMap<>();
+        this.brackets.put(')', '(');
+        this.brackets.put(']', '[');
+    }
+
+    public String getExpression() {
+        return expression;
     }
 
     //публичный метод, который будет использоваться в другом классе после всех проверок.
     public boolean isExpressionValid() {
-        return isElementOrderCorrect();
+        return isBracketsOrderCorrect();
 
     }
 
@@ -38,23 +44,45 @@ public class DateValidator {
     /**
      * Метод проверки порядка токенов в пользовательском выражении.
      */
-    private boolean isElementOrderCorrect() {
-        Deque<Character> stack = new ArrayDeque<>();
+    private boolean isBracketsOrderCorrect() {
+        Deque<Character> stack = new LinkedList<>();
         if (isValidTokens()) {
-            //ниже логика проверки порядка токенов в пользовательском выражении.
 
+            //ниже логика проверки вложенности скобок в пользовательском выражении.
             for (char c : expression.toCharArray()) {
-                if (c == '(' || c == '+' || c == '-' || c == '*' || c == '/') {
-                    stack.addFirst(c);
-                } else if (c == ')') {
-                    if (stack.isEmpty() || stack.peekFirst() != '(') {
+                if (brackets.containsValue(c)) {
+                    stack.push(c);
+                } else if (brackets.containsKey(c)) {
+                    if (stack.isEmpty() || stack.pop() != brackets.get(c)) {
                         return false;
                     }
-                    stack.removeFirst(); // удаляем '(' из стека
                 }
             }
+            return stack.isEmpty();
         }
-        return stack.isEmpty();
+        return false; //здесь была ошибка из-за того, что в мапе скобок были квадратные скобки, а в списке токенов - нет.
+    }
+
+    /**
+     * Ниже повтор метода, просто чтобы визуализировать стек.
+     */
+    public Deque<Character> isBracketsOrderCorrectViewer() {
+        Deque<Character> stack = new LinkedList<>();
+        if (isValidTokens()) {
+            //ниже логика проверки вложенности скобок в пользовательском выражении.
+
+            for (char c : expression.toCharArray()) {
+                if (brackets.containsValue(c)) {
+                    stack.push(c);
+                } else if (brackets.containsKey(c)) {
+                    if (stack.isEmpty() || stack.pop() != brackets.get(c)) {
+                        return stack;
+                    }
+                }
+            }
+            return stack;
+        }
+        return stack;
     }
 
 
--- JavaCourse/src/OOP/solid/View.java
+++ JavaCourse/src/OOP/solid/View.java
@@ -1,7 +1,5 @@
 package OOP.solid;
 
-import java.util.Deque;
-import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -10,14 +8,27 @@ import java.util.Scanner;
 public class View {
     private static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        start();
+        //(1+2)*3
+        DateValidator dv = new DateValidator(prompt());
+        System.out.println(dv.getExpression());
+        System.out.println(dv.isExpressionValid());
+        System.out.println(dv.isBracketsOrderCorrectViewer());
+        //где-то либо создать класс, либо метод для добавления пробелов (есть в буфере).
+        //также нужно сделать проверку число или оператор или скобка, скорее всего просто через проверку приоритета.
+
+//        Deque<Integer> stack2 = new ArrayDeque<>();
+//        stack2.add(1);
+//        stack2.add(2);
+//        stack2.addLast(0);
+//        stack2.addFirst(100);
+//        System.out.println(stack2);
     }
 
-    private static void start(){
-        while (true){
-            prompt();
-        }
-    }
+//    private static void start(){
+//        while (true){
+//            prompt();
+//        }
+//    }
     private static String prompt(){
         System.out.print("Введите выражение: ");
         return sc.nextLine();
